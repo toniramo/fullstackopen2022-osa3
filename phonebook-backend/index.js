85dbe893b669bf86c5bc1a2018dca0f87b1af462
Toni Rämö <toni.ramo@helsinki.fi>
2022-08-24 10:58:51 +0300

    palauta 3.12-3.14

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+require('dotenv').config();
+const Person = require('./models/person');
 
 const app = express();
 app.use(express.json());
@@ -11,6 +13,7 @@ morgan.token('req-body', function getPostData(req, res) {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body'));
 
+/*
 let persons = [
     {
         "name": "Arto Hellas",
@@ -32,31 +35,40 @@ let persons = [
         "number": "39-23-6423122",
         "id": 4
     }
-];
+];*/
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons);
+    Person.find({}).then(persons => response.json(persons));
 });
 
 app.get('/api/info', (request, response) => {
-    const info1 = `<p>Phonebook has info for ${persons.length} people.</p>`;
-    const info2 = `<p>${new Date()}</p>`;
-    response.send(info1.concat(info2));
+    Person.find({}).then(persons => {
+        const info1 = `<p>Phonebook has info for ${persons.length} people.</p>`;
+        const info2 = `<p>${new Date()}</p>`;
+        response.send(info1.concat(info2));
+    });
 });
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id);
-    const person = persons.find(person => person.id === id);
-    person ? response.json(person) : response.status(404).end();
-})
+    const id = request.params.id;
+    Person.findById(id).then(person => {
+        person ? response.json(person) : response.status(404).end();
+    })
+});
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id);
-    if (!persons.find(person => person.id === id)) {
-        return response.status(404).end();
-    }
-    persons = persons.filter(person => person.id !== id);
-    response.status(204).end();
+    const id = request.params.id;
+    Person.findByIdAndDelete(id).then(person => {
+        if (!person) {
+            return response.status(404).end();
+        }
+        response.status(204).end()
+    });
+    //    if (!persons.find(person => person.id === id)) {
+    //       return response.status(404).end();
+    //  }
+    //  persons = persons.filter(person => person.id !== id);
+    //  response.status(204).end();
 })
 
 app.post('/api/persons', (request, response) => {
@@ -64,21 +76,37 @@ app.post('/api/persons', (request, response) => {
     if (!body.name || !body.number) {
         return response.status(400).send({ error: 'person must have a name and a number' });
     }
-    if (persons.find(person => person.name === body.name)) {
+    /*if (persons.find(person => person.name === body.name)) {
         return response.status(409).send({ error: 'name already exists in phonebook' })
-    }
-    const person = {
-        name: body.name,
-        number: body.number,
-        id: generateId()
-    };
-    persons = persons.concat(person);
-    response.json(person);
-})
+    }*/
+    Person.find({ name: body.name }).then(foundPerson => {
+        if (foundPerson.length) {
+            return response.status(409).send({ error: 'name already exists in phonebook' });
+        }
+        const person = new Person({
+            name: body.name,
+            number: body.number
+        });
+        person.save().then(savedPerson => {
+            response.json(savedPerson);
+        })
+    })
+    /*
+       const person = new Person({
+           name: body.name,
+           number: body.number
+       });
+   
+   
+       person.save().then(savedPerson => {
+           persons = persons.concat(savedPerson);
+           response.json(savedPerson);
+       });*/
+});
 
-const generateId = () => {
-    return Math.floor(Math.random() * 1e6) + 1;
-}
+//const generateId = () => {
+//    return Math.floor(Math.random() * 1e6) + 1;
+//}
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
---
+++ phonebook-backend/mongo.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+
+if (process.argv.length < 3) {
+    console.log('you must give password as an argument');
+    process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url =
+    `mongodb+srv://fullstackopen22:${password}@cluster0.6wxtcyj.mongodb.net/phonebookApp?retryWrites=true&w=majority`;
+
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+});
+
+const Person = mongoose.model('Person', personSchema);
+
+const addPerson = (name, number) => {
+
+    console.log("adding new person", name, number);
+    const person = new Person({
+        name: name,
+        number: number
+    });
+
+    console.log('person :>> ', person);
+
+    person
+        .save()
+        .then(result => {
+            console.log('person saved.');
+            console.log('result :>> ', result);
+            mongoose.connection.close();
+            return result;
+        }).catch(error => console.log('error :>> ', error));
+}
+
+const getAll = () => {
+    console.log('Phonebook:')
+    Person
+        .find({})
+        .then(result => {
+            result.forEach(person => console.log(person.name, person.number));
+            mongoose.connection.close();
+        })
+}
+
+if (process.argv.length > 4) {
+    const name = process.argv[3];
+    const number = process.argv[4];
+
+    return addPerson(name, number);
+}
+
+getAll();
